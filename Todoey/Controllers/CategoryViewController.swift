0673a246023737a4f4aeb616f9b1bd3b01e216ae
pedro <pfreitas@id.uff.br>
2020-12-23 17:54:01 -0300

    add search bar

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Pedro Freitas | Zoop on 23/12/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK - TableView DataSource Methods
+    
+    //MARK - Data Manipulation Methods
+    
+    //MARK - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
+    //MARK - TableView Delegate Methods
+    
+    
+    
+}
